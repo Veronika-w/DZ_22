2f86fd8dc95bf7d6519d273074f5a0d89f43c44d
Вероника Мазуренко <veronikavv1732@gmail.com>
2026-03-20 16:18:08 +0300

    add funcs and base

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def  home(request):
@@ -7,3 +9,19 @@ def  home(request):
 
 def  contacts(request):
     return render(request, 'contacts.html')
+
+
+def product_list(request):
+    catalogs = Product.objects.all()
+    context = {"catalogs": catalogs}
+    return render(request, 'product_list.html', context)
+
+
+def get_objects_or_404(Product, pk):
+    pass
+
+
+def product_detail(request, pk):
+    catalog = get_object_or_404(Product, pk=pk)
+    context = {"catalog": catalog}
+    return render(request, 'product_detail.html', context)
