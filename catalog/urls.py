bb7623ada968b41f6b9d656be483d329a3ec7567
Вероника Мазуренко <veronikavv1732@gmail.com>
2026-03-26 20:25:47 +0300

    add forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # path('contacts/', contacts, name='contacts'),
     path('', ProductListView.as_view(), name='product_list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('products/create', ProductCreateView.as_view(), name='product_create'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
 ]
