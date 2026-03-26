17cf357175dfaf739b8f9b922bbb485d349ceb85
Вероника Мазуренко <veronikavv1732@gmail.com>
2026-03-26 20:26:00 +0300

    add forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,8 +5,6 @@ from catalog.views import ProductListView, ProductDetailView, ProductCreateView,
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    # path('', home, name='home'),
-    # path('contacts/', contacts, name='contacts'),
     path('', ProductListView.as_view(), name='product_list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('products/create/', ProductCreateView.as_view(), name='product_create'),
